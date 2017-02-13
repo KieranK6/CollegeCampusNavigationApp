5824ad7aeee1f6b7958e8bd10568aca52341f986
KieranK <kierank695@gmail.com>
2017-02-13 11:38:20 +0000

    Added Twitter and Timetable redirect functionality to main menu

---
+++ Assets/Scripts/SocialFeed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialFeed : MonoBehaviour {
+
+	public void twitter()
+	{
+		Application.OpenURL("https://twitter.com/dkitsu?lang=en");
+	}
+}
---
+++ Assets/Scripts/TimetableRedirect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimetableRedirect : MonoBehaviour {
+
+	public void redirect()
+	{
+		Application.OpenURL("https://timetables.dkit.ie/");
+	}
+}
